a3726ab64afd01970103a5ff60606d9f9c08bc9f
whiteBULLET <734708156@qq.com>
2024-04-22 13:30:35 +0800

    4_19_version

---
+++ DAS_2.py
@@ -0,0 +1,34 @@
+import numpy as np
+from scipy import signal
+from nptdms import TdmsFile
+import h5py
+import segyio
+
+
+
+def time_span(dir):
+    timespanlist = []
+    
+
+    return timespanlist
+
+def cat_time_span_data(dir,timespan, downsample = 3):
+    startTime , endTime = timespan
+
+
+
+
+def das_downsample(data ,  degree = 3):
+
+
+
+    return data
+
+
+def pic_fix_downsample(data,size):
+
+
+
+def general_analysis(dir):
+
+    
